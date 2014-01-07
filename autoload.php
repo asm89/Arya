a0b8f1fd4cccfcf0823a10970bbb19ea9a66b2fd
Daniel Lowrey <rdlowrey@php.net>
2014-01-06 20:20:37 -0700

    Added missing ASGI autoloader inclusion

--- autoload.php
+++ autoload.php
@@ -2,7 +2,7 @@
 
 spl_autoload_register(function($class) {
     if (0 === strpos($class, 'Arya\\')) {
-        $class = str_replace('\\', DIRECTORY_SEPARATOR, $class);
+        $class = str_replace('\\', '/', $class);
         $file = __DIR__ . "/lib/{$class}.php";
         if (file_exists($file)) {
             require $file;
@@ -11,3 +11,5 @@ spl_autoload_register(function($class) {
 });
 
 require __DIR__ . '/vendor/Auryn/autoload.php';
+require __DIR__ . '/vendor/Asgi/autoload.php';
+
